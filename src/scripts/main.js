d9fdc02092e790c5acdd342604d1491cfe30dee8
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-04-25 18:29:40 +0200

    list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,3 @@
-const toDo = () => {
-  let userInput = document.querySelector("#userText").value;
-  console.log(userInput);
-  let newList = document.createElement("li");
-  let text = document.createTextNode(userInput);
-
-  newList.appendChild(text);
-  document.querySelector(".result").appendChild(newList);
-  document.querySelector("#userText").value = "";
-
-  let oddItems = document.querySelectorAll("li:nth-child(odd)");
-  console.log(oddItems);
-  oddItems.forEach(({ style: item }) => {
-    item.backgroundColor = color();
-  });
-};
-
 const color = () => {
   let result = "#";
   let HexNum = "0123456789ABCDEF";
@@ -23,4 +6,35 @@ const color = () => {
   }
   console.log(result);
   return result;
+};
+
+const toDo = () => {
+  let userInput = document.querySelector("#userText").value;
+  console.log(userInput);
+
+  if (userInput == "") {
+    return alert("Write something please");
+  }
+
+  let newList = document.createElement("li");
+  let text = document.createTextNode(userInput);
+  newList.appendChild(text);
+  document.querySelector(".result").appendChild(newList);
+  document.querySelector("#userText").value = "";
+
+  let oddItems = document.querySelectorAll("li:nth-child(odd)");
+  console.log(oddItems);
+  oddItems.forEach(({ style: item }) => {
+    item.color = color();
+  });
+
+  // let span = document.createElement("span");
+  // let text = document.createTextNode("x");
+  // span.className = "close";
+  // span.appendChild(text);
+
+  // let listAll = document.querySelectorAll("li");
+  // listAll.forEach((item) => {
+  //   item.appendChild(span);
+  // });
 };
